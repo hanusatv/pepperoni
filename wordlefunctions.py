eda6d80ce82612f5b1eb2e1dcf1acb829c816063
hanusatv <hanusatv@gmail.com>
2022-04-06 00:30:58 +0200

    os á DB fílar

--- wordlefunctions.py
+++ wordlefunctions.py
@@ -6,14 +6,14 @@ import os
 DB_DIR = "static/database"
 
 # Ger ein lista við øllum valid guesses
-file = open(os.path.join(DB_DIR, "validGuesses.json"), encoding="utf-8")
+file = open("static/database/validGuesses.json", encoding="utf-8")
 validGuesses = json.load(file)
 file.close()
 a = (map(lambda x: x.lower(), validGuesses))
 validGuesses = list(a)
 
 # Ger ein lista við øllum valid wordl
-file = open(os.path.join(DB_DIR, "wordlist.json"), encoding="utf-8")
+file = open("static/database/wordlist.json", encoding="utf-8")
 wordlist = json.load(file)
 file.close()
 a = (map(lambda x: x.lower(), wordlist))
